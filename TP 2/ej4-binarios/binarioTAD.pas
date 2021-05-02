8feb4cecc8c52a7c091562016a1d0433690075e8
JosueGatica <josuegaticaodato@gmail.com>
2021-05-02 12:28:46 -0300

    Realizo Suma y Operaciones Logicas

--- TP 2/ej4-binarios/binarioTAD.pas
+++ TP 2/ej4-binarios/binarioTAD.pas
@@ -11,20 +11,20 @@ type
     nroBinario: String;
   public
     function Crear(nro: string) : boolean;
-    function sumar(nro: string) : string;
-    function toStr(nro: integer) : string;
-    {function restar(nro) : string;
+    function Sumador(nro: string) : Binario;
+    function toStr() : string;
+    //function restar(nro) : string;
     function convertirADecimal(nro) : string;
-    function andLogico(nro) : string;
-    function orLogico(nro) : string;
-    function xorLogico(nro) : string;}
+    function andLogico(nro: String) : Binario;
+    function orLogico(nro: String) : Binario;
+    function xorLogico(nro: String) : Binario;
   End;
 
 implementation
 
-function Binario.toStr(nro: integer) : string;
+function Binario.toStr() : string;
 begin
-  toStr := nro.ToString;
+  toStr := nroBinario;
 end;
 
 function Binario.Crear(nro: string) : boolean;
@@ -33,82 +33,282 @@ begin
   nroBinario := nro;
   Result := True;
   for I := 1 to Length(nro) do begin
-    if (nro[I] <> '0') or (nro[I] <> '1') then begin
+    if (nro[I] <> '0') and (nro[I] <> '1') then begin
       Result := False;
     end;
   end;
 end;
 
-function masLargo(nro1, nro2: string) : string;
+//Procedimiento interno que empareja los numeros, agregandole 0, para realizar la suma correspondiente
+procedure Emparejar(var Mas_Largo, Mas_Corto: String);
+Var Cantidad, i : Integer;
 begin
-  masLargo := 'Igual longitud';
+  //Determino cual es la diferencia que hay entre el mas largo y el mas corto
+  Cantidad := Length(Mas_Largo) - Length(Mas_Corto);
+  for i := 1 to Cantidad do
+  begin
+    //Agrego los 0 necesarios para emparejar ambos numeros
+    Mas_corto := '0' + Mas_corto;
+  end;
+end;
+
+//Funcion que determina cual es el numero mas largo
+function masLargo(var nro1, nro2: string) : Integer;
+var Longitud : Integer;
+begin
   if Length(nro1) > Length(nro2) then begin
-    masLargo := nro1;
+    //Si uno es mas largo que otro, se determina el valor
+    //mas largo y se empareja el numero mas corto
+    Longitud := Length(nro1);
+    Emparejar(nro1,nro2);
   end
   else if Length(nro1) < Length(nro2) then begin
-    masLargo := nro2;
+    //Si uno es mas largo que otro, se determina el valor
+    //mas largo y se empareja el numero mas corto
+    Longitud := Length(nro2);
+    Emparejar(nro2,nro1);
+  end
+  else
+  begin
+    //En el caso de que no halla diferencia y sean de igual longitud
+    //Elijo cualquiera de los dos numeros y no realizo el emparejamiento
+    Longitud := Length(nro1);
   end;
+  masLargo := Longitud;
 end;
 
-function Binario.sumar(nro: string) : string;
-var I: Integer;
-    suma, carry, larger: String;
+//Funcion que realiza la suma binaria
+function Binario.Sumador(nro: string) : Binario;
+//Determino las variables
+var I, Mas_largo, Acumulador : Integer;
+    Carry, Suma: String;
 begin
-  //llamo a la función para chequear longitud (para poder recorrer parejo con los indices)
-  larger := masLargo(nroBinario, nro);
-
-  // inicializo las var suma y carry con ceros
-  //el primer lugar de suma se reserva por si queda carry
-  for I := 1 to Length(larger) + 1 do begin
-    suma := suma + '0';
-    carry := carry + '0';
-  end;
-
-  //si uno num es mas largo que otro voy sumando como si el mas chico tuviera ceros
-  if larger <> 'Igual longitud' then begin
-    for I := Length(nroBinario) downto Length(nro) do begin
-      suma[I+1] := nroBinario[I];
-    end;
-  end;
-
-  //hago la suma arrancando desde atras
-  for I := Length(nro) downto 1 do begin
-  //si es 1 y 1 el carry va a ser 1, si ya había carry la suma va a ser 1, sino la suma será 0
-    if (nroBinario[I] = '1') and (nro[I] = '1') then begin
-      carry[I+1] := '1';
-      if carry[I-1] = '1' then begin
-        suma[I+1] := '1';
-      end
-      else begin
-        suma[I+1] := '0';
-      end;
-    end
-  //si son diferentes, depende del carry, si había un 1, suma será 0, si había un 0, suma será 1
-    else if ((nroBinario[I] = '1') and (nro[I] = '0')) or ((nroBinario[I] = '0') and (nro[I] = '1')) then begin
-      if carry[I-1] = '1' then begin
-        suma[I+1] := '0';
-        carry[I+1] := '1';
-      end
-      else begin
-        suma[I+1] := '1';
-      end;
-    end
-    //si son 0 y 0
-    else begin
-      if carry[I-1] = '1' then begin
-        suma[I+1] := '1';
-      end
-      else begin
-        suma[I+1] := '0';
+  //Determino cual es el numero mas largo y inicializo las variables
+  Mas_largo := masLargo(nroBinario, nro);
+  Carry := '0';
+  Suma := '';
+  //Recorro los strings de atras para adelante (por eso el downto)
+  for I := Mas_largo + 1 downto 1 do
+  begin
+    // Si es 1 + 0, entonces:
+    if (nroBinario[I] = '1') and (nro[I] = '0')  then
+    begin
+      //Si el carry es 1:
+      if Carry = '1' then
+        begin
+        //Entonces voy a tener como valor de suma 0, y el carry le sumo 1
+        Suma := '0' + Suma;
+        Carry := '1';
+        end
+      else
+      begin
+      //Sino es 1 el carry, pongo el 1 a la suma (1+0 en binario es 1)
+        Suma := '1' + Suma;
       end;
     end;
-  end;
-  //termina el for y si quedo carry tengo que agregar un 1 al principio de suma
-  if carry[I] = '1' then begin
-    suma[I] := '1';
+    //Si 0 + 1
+    if (nroBinario[I] = '0') and (nro[I] = '1')  then
+    begin
+      //Si el carry es 1 entonces
+      if Carry = '1' then
+        begin
+        //A la suma le coloco 0 y el carry sigue siendo 1
+        Suma := '0' + Suma;
+        Carry := '1';
+        end
+      else
+      begin
+        //Si el carry no es 1, realizo la suma (0+1 en binario es 1)
+        Suma := '1' + Suma;
+      end;
+    end;
+    // Si 0 + 0
+    if (nroBinario[I] = '0') and (nro[I] = '0')  then
+    begin
+      //Si el carry es 1
+      if Carry = '1' then
+        begin
+        //Gasto el carry en la suma y este se trasnforma en 0
+        Suma := '1' + Suma;
+        Carry := '0';
+        end
+      else
+      begin
+        //Sino, realizo la suma binaria comun (0+0 en binario es 0)
+        Suma := '0' + Suma;
+      end;
+    end;
+    // Si 1+1
+    if (nroBinario[I] = '1') and (nro[I] = '1')  then
+    begin
+      //Si el carry es 1
+      if Carry = '1' then
+        begin
+        //La suma entre ellos da 3(11 en binario), entonces pongo 1 en la suma y le coloco 1 al carry
+        Suma := '1' + Suma;
+        Carry := '1';
+        end
+      else
+      begin
+        //Sino, realizo la suma que es 2(1+1:10), el 0 se queda en la suma y el carry se transforma en 1
+        Suma :='0' + Suma;
+        Carry := '1';
+      end;
+    end;
+
   end;
 
+  //En el caso de que en el carry haya sobrado algo, lo pongo adelante en la suma
+  if Carry = '1' then
+  begin
+    Suma := '1' + Suma;
+  end;
+
+  //Devuelvo el resultado de la suma
+ Sumador.nroBinario := Suma;
 end;
 
+//------------------------------------//
+function Binario.convertirADecimal(nro) : string;
+
+
+//------------------------------------//
+
+//Procedimiento que realiza el emparejamiento de bits para las operaciones logicas
+procedure Emparejar_logico(var Mas_Largo, Mas_Corto: String);
+Var Cantidad, i : Integer;
+begin
+  //Determino cual es la diferencia que hay entre el mas largo y el mas corto
+  Cantidad := Length(Mas_Largo) - Length(Mas_Corto);
+  for i := 1 to Cantidad do
+  begin
+    //Agrego giones necesarios para emparejar ambos numeros
+    Mas_corto := '-' + Mas_corto;
+  end;
+end;
+
+//Funcion que determina cual es el numero mas largo (PARA OPERACIONES LOGICAS)
+function masLargo_Logico(var nro1, nro2: string) : Integer;
+var Longitud : Integer;
+begin
+  if Length(nro1) > Length(nro2) then begin
+    //Si uno es mas largo que otro, se determina el valor
+    //mas largo y se empareja el numero mas corto
+    Longitud := Length(nro1);
+    Emparejar_logico(nro1,nro2);
+  end
+  else if Length(nro1) < Length(nro2) then begin
+    //Si uno es mas largo que otro, se determina el valor
+    //mas largo y se empareja el numero mas corto
+    Longitud := Length(nro2);
+    Emparejar_logico(nro2,nro1);
+  end
+  else
+  begin
+    //En el caso de que no halla diferencia y sean de igual longitud
+    //Elijo cualquiera de los dos numeros y no realizo el emparejamiento
+    Longitud := Length(nro1);
+  end;
+  masLargo_Logico := Longitud;
+end;
+
+//Funcion que realiza la operacion AND logico
+function Binario.andLogico(nro: String) : Binario;
+//Determino las variables
+var I, Mas_largo : Integer;
+    Resultado: String;
+begin
+  //Determino cual es el numero mas largo y inicializo las variables
+  Mas_largo := masLargo_Logico(nroBinario, nro);
+  I := (Mas_largo);
+  //Mientras no halla guiones y todavia halla bits, continuo haciendo el and
+  while (I >= 1) and (nroBinario[I] <> '-') and (nro[I] <> '-') do
+    begin
+      begin
+        //Si tengo 1-1
+        if (nroBinario[I] = '1') and (nro[I] = '1')  then
+        begin
+        //Entonces realizo el AND ( 1-1 : 1) y lo coloco como resultado
+        Resultado := '1' + Resultado;
+        end
+        else
+        begin
+        //Cualquier otro resultado, el AND devuelve 0
+        //Por lo tanto, coloco 0 como resultado y concateno
+        Resultado := '0' + Resultado;
+        end;
+      end;
+    //Resto la variable interactiva
+    I := I - 1;
+    end;
+//Devuelvo el resultado a el elemento del objeto
+andLogico.nroBinario := Resultado;
+end;
+
+//Funcion que realiza la operacion OR logico
+function Binario.orLogico(nro: String) : Binario;
+//Determino las variables
+var I, Mas_largo : Integer;
+    Resultado: String;
+begin
+  //Determino cual es el numero mas largo y inicializo las variables
+  Mas_largo := masLargo_Logico(nroBinario, nro);
+  I := (Mas_largo);
+  //Mientras no halla guiones y todavia halla bits, continuo haciendo el or
+  while (I >= 1) and (nroBinario[I] <> '-') and (nro[I] <> '-') do
+    begin
+      begin
+        //Si tengo 0-0
+        if (nroBinario[I] = '0') and (nro[I] = '0')  then
+        begin
+        //Entonces realizo el OR ( 0-0 : 0) y lo coloco como resultado
+        Resultado := '0' + Resultado;
+        end
+        else
+        begin
+        //Cualquier otro resultado, el OR devuelve 1
+        //Por lo tanto, coloco 1 como resultado y concateno
+        Resultado := '1' + Resultado;
+        end;
+      end;
+    //Resto la variable interactiva
+    I := I - 1;
+    end;
+//Devuelvo el resultado a el elemento del objeto
+orLogico.nroBinario := Resultado;
+end;
+
+//Funcion que realiza la operacion XOR logico
+function Binario.xorLogico(nro: String) : Binario;
+//Determino las variables
+var I, Mas_largo : Integer;
+    Resultado: String;
+begin
+  //Determino cual es el numero mas largo y inicializo las variables
+  Mas_largo := masLargo_Logico(nroBinario, nro);
+  I := (Mas_largo);
+  //Mientras no halla guiones y todavia halla bits, continuo haciendo el xor
+  while (I >= 1) and (nroBinario[I] <> '-') and (nro[I] <> '-') do
+    begin
+      begin
+        //Si tengo 1-0 O 0-1
+        if ((nroBinario[I] = '0') and (nro[I] = '1')) or ((nroBinario[I] = '1') and (nro[I] = '0'))  then
+        begin
+        //Entonces realizo el XOR ( 1-0 o 0-1 : 1) y lo coloco como resultado
+        Resultado := '1' + Resultado;
+        end
+        else
+        begin
+        //Cualquier otro resultado, el XOR devuelve 0
+        //Por lo tanto, coloco 1 como resultado y concateno
+        Resultado := '0' + Resultado;
+        end;
+      end;
+    //Resto la variable interactiva
+    I := I - 1;
+    end;
+//Devuelvo el resultado a el elemento del objeto
+xorLogico.nroBinario := Resultado;
+end;
 
 end.
--- TP 2/ej4-binarios/ejercicio4Binarios.dfm
+++ TP 2/ej4-binarios/ejercicio4Binarios.dfm
@@ -1,8 +1,8 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
-  ClientHeight = 344
+  Caption = 'Ejercicio 4'
+  ClientHeight = 320
   ClientWidth = 628
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -11,52 +11,98 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
   PixelsPerInch = 96
   TextHeight = 13
   object Label1: TLabel
-    Left = 27
-    Top = 32
-    Width = 126
+    Left = 64
+    Top = 37
+    Width = 145
     Height = 13
+    Alignment = taCenter
     Caption = 'Ingrese un n'#250'mero binario'
   end
   object Label2: TLabel
-    Left = 27
-    Top = 106
-    Width = 134
+    Left = 64
+    Top = 100
+    Width = 145
     Height = 13
+    Alignment = taCenter
     Caption = 'Ingrese otro n'#250'mero binario'
   end
   object Memo1: TMemo
-    Left = 208
-    Top = 29
-    Width = 297
-    Height = 177
+    Left = 280
+    Top = 8
+    Width = 329
+    Height = 291
     Lines.Strings = (
       'Memo1')
     TabOrder = 0
   end
   object binario1: TEdit
-    Left = 32
+    Left = 64
     Top = 64
-    Width = 121
+    Width = 145
     Height = 21
     TabOrder = 1
   end
   object binario2: TEdit
-    Left = 32
-    Top = 125
-    Width = 121
+    Left = 64
+    Top = 123
+    Width = 145
     Height = 21
     TabOrder = 2
   end
   object btnSuma: TButton
-    Left = 32
-    Top = 168
+    Left = 40
+    Top = 169
     Width = 75
     Height = 25
     Caption = 'Suma binaria'
     TabOrder = 3
     OnClick = btnSumaClick
+  end
+  object btnResta: TButton
+    Left = 152
+    Top = 169
+    Width = 75
+    Height = 25
+    Caption = 'Resta Binaria'
+    TabOrder = 4
+  end
+  object btnConversion: TButton
+    Left = 96
+    Top = 208
+    Width = 75
+    Height = 25
+    Caption = 'Conversion'
+    TabOrder = 5
+  end
+  object btnAND: TButton
+    Left = 15
+    Top = 256
+    Width = 75
+    Height = 25
+    Caption = 'AND'
+    TabOrder = 6
+    OnClick = btnANDClick
+  end
+  object btnOR: TButton
+    Left = 96
+    Top = 256
+    Width = 75
+    Height = 25
+    Caption = 'OR'
+    TabOrder = 7
+    OnClick = btnORClick
+  end
+  object btnXOR: TButton
+    Left = 177
+    Top = 256
+    Width = 75
+    Height = 25
+    Caption = 'XOR'
+    TabOrder = 8
+    OnClick = btnXORClick
   end
 end
