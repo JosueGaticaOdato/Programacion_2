1b37b36a28a0393088d05d34cf4b7e63e46c4f8a
josuegaticaodato@gmail.com <JosueGatica>
2021-06-14 11:50:33 -0300

    Ejercicio 9 cargado

--- TP 5/Ejercicio 5/CodigoEjercicio5.pas
+++ TP 5/Ejercicio 5/CodigoEjercicio5.pas
@@ -16,9 +16,11 @@ type
     Button2: TButton;
     Memo1: TMemo;
     Edit1: TEdit;
+    Button3: TButton;
     procedure Button1Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
     procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
     { Private declarations }
     Pila: Ejercicio5;
@@ -46,6 +48,20 @@ begin
   //PSinOcurrencias.Definir_Tamaño_Pila(CantidadMaxRandom);
   X.Clave := StrToint(Edit1.Text);
   PSinOcurrencias := Pila.Eliminar_Ocurrencias(X);
+  memo1.Lines.Add('La pila sin las ocurrencias es');
+  memo1.Lines.Add(PSinOcurrencias.Mostrar_Pila);
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var X: TipoElemento;
+  PSinOcurrencias: Ejercicio5;
+  AUX1,AUX2,AUX3: Ejercicio5;
+begin
+  Aux1.Definir_Tamaño_Pila(CantidadMaxRandom);
+  Aux2.Definir_Tamaño_Pila(CantidadMaxRandom);
+  Aux3.Definir_Tamaño_Pila(CantidadMaxRandom);
+  X.Clave := StrToint(Edit1.Text);
+  PSinOcurrencias := Pila.Eliminar_Ocurrencias_Recursiva(X,AUX1,AUX2,AUX3);
   memo1.Lines.Add('La pila sin las ocurrencias es');
   memo1.Lines.Add(PSinOcurrencias.Mostrar_Pila);
 end;
